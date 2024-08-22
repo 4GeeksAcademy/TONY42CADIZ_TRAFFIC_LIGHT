a4a0111c68d70d9331ced191d96df569efe6fcd8
ANTONIO MANUEL MOLINA CAÑETE <MOLFERGUELSA@HOTMAIL.COM>
2024-08-22 16:35:18 +0000

    First Commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,13 @@
 //import react into the bundle
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+import TrafficLight from "./component/TrafficLight.jsx";
 
 //render your react application
-ReactDOM.createRoot(document.getElementById('app')).render(<Home/>);
+ReactDOM.render( < TrafficLight / > , document.querySelector("#app"));
 
